35dfdb316d2c73ba96c1dbb6b63e60f1bfd2314c
Armando Ibarra1 <armandoibarra1@gmail.com>
2014-05-19 12:53:36 -0600

    Intro for agent for os to send or receive data eg exec commands

---
+++ erpmtics-agent.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*- 
+
+###############################################################################
+# Copyright (C) 2014 Armando Ibarra
+# Agent to receive data from server or send data from client to server v0.1 alpha - 2014-May-19
+# 
+
+#----------------------------------------------------------------------
+# erpmtics-agent.py
+#
+# Created: 19/05/2014
+#
+# Author: Ing. Armando Ibarra - armandoibarra1@gmail.com
+#----------------------------------------------------------------------
+
+# This script search data and send to server for example to know the status 
+# clinto (connected, shuttdown, etc) connecting to web server 
+
+# Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl-2.1.html
+# - or any later version.
+# @author: Ing. Armando Ibarra
+
+
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+###############################################################################
+
+
+def test():
+	print "Hello"
+
+if __name__=='__main__':
+	# Setup the 2 contexts.
+	#get_category_links(BASE_URL)
+    tracks = test()
