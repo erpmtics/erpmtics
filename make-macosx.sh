2d6b30c86a10e927be1d589f5c2b926808a2800e
Armando Ibarra1 <armandoibarra1@gmail.com>
2014-05-19 10:46:50 -0600

    made a change on some files

---
+++ make-macosx.sh
@@ -0,0 +1,4 @@
+#Install Homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+#Install sshpass
+brew install https://raw.github.com/eugeneoden/homebrew/eca9de1/Library/Formula/sshpass.rb
